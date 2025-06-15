5074cbe2706ef8cca2fdb8b352ab163296230501
jamesc618 <christodouloujames@gmail.com>
2025-06-15 21:31:24 +0300

    Update app.py

--- app.py
+++ app.py
@@ -41,5 +41,6 @@ def index():
 
 if __name__ == '__main__':
     app.run(debug=True)
-git commit -m "Add model.pkl"
+git add app.py
+git commit -m "Removed invalid git command"
 git push origin main
